9e83e98f1b181835ee25b6eee5651ac9982f8b59
Hrishit Jangra <j4i1310@gmail.com>
2023-10-03 21:27:42 +0530

    Changed app2.py to include fuel price for 4 main hubs,

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-
+import numpy.array as array
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 data = pd.read_csv('FDCA320neo.csv')
 
-X = data[['Distance', 'Altitude', 'WeightTO', 'ZFW']]
+X = data[['Distance', 'Altitude', 'ZFW']]               #ZFW is the Zero-Fuel Weight, as in the weight of the aircraft without any fuel on board
 y = data['Total Fuel']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
@@ -22,13 +22,37 @@ def index():
 
 @app.route('/calculate_fuel', methods=['POST'])
 def calculate_fuel():
-    distance = int(request.form['distance'])
-    altitude = int (request.form['altitude'])
-    ZFW = int(request.form['ZFW'])
+    
+    #A320neo uses Jet A-1 type fuel, the density is approx 0.8037 kg/L
+    
+    fuel_density = 0.8037
+    
+    distance = int(request.form['distance'])            #Distance is in nm -> Nautical Miles
+    altitude = int (request.form['altitude'])           #Altitude is in ft -> Feet
+    ZFW = int(request.form['ZFW'])                      #ZFW is in kg -> kilogram
     
     fuel_req = rf_model.predict([[distance,altitude, ZFW]])[0]
+    fuel_volume = fuel_req / fuel_density
     
-    return render_template('result.html', fuel_req=fuel_req)
+    #Current fuel price given in rupees per kilo litre, taken from https://iocl.com/aviation-fuel gives price for 4 main airline hubs of india: mumbai, delhi, chennai, kolkata
+    fuel_mumbai = 110592.31
+    fuel_delhi = 118199.17
+    fuel_kolkata = 126697.08
+    fuel_chennai = 122423.92
+    
+    fuel_prices = {
+    'Mumbai': fuel_mumbai,
+    'Delhi': fuel_delhi,
+    'Kolkata': fuel_kolkata,
+    'Chennai': fuel_chennai
+    }
+    
+    fuel_cost = fuel_volume * array(list(fuel_prices.values()))
+
+    # Format the fuel cost for each location
+    formatted_fuel_costs = {place: f'{cost:.2f} Rupees' for place, cost in zip(fuel_prices.keys(), fuel_cost)}
+
+    return render_template('result.html', fuel_req=fuel_req, fuel_volume=fuel_volume, fuel_cost=formatted_fuel_costs)
 
 if __name__ == '__main__':
     app.run(debug=True)
