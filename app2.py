1a6b304e441f18930ab1674c223fa8fe0e3a1352
Hrishit Jangra <j4i1310@gmail.com>
2023-10-04 09:03:49 +0530

    Added error handling and fixed the value formats for results.html

--- app2.py
+++ app2.py
@@ -23,36 +23,55 @@ def index():
 @app.route('/calculate_fuel', methods=['POST'])
 def calculate_fuel():
     
-    #A320neo uses Jet A-1 type fuel, the density is approx 0.8037 kg/L
-    
-    fuel_density = 0.8037
-    
     distance = int(request.form['distance'])            #Distance is in nm -> Nautical Miles
     altitude = int (request.form['altitude'])           #Altitude is in ft -> Feet
     ZFW = int(request.form['ZFW'])                      #ZFW is in kg -> kilogram
     
-    fuel_req = rf_model.predict([[distance,altitude, ZFW]])[0]
-    fuel_volume = fuel_req / fuel_density
-    
-    #Current fuel price given in rupees per kilo litre, taken from https://iocl.com/aviation-fuel gives price for 4 main airline hubs of india: mumbai, delhi, chennai, kolkata
-    fuel_mumbai = 110592.31
-    fuel_delhi = 118199.17
-    fuel_kolkata = 126697.08
-    fuel_chennai = 122423.92
-    
-    fuel_prices = {
-    'Mumbai': fuel_mumbai,
-    'Delhi': fuel_delhi,
-    'Kolkata': fuel_kolkata,
-    'Chennai': fuel_chennai
-    }
-    
-    fuel_cost = fuel_volume * np.array(list(fuel_prices.values()))
+    try:
+        if (ZFW >= 57000 and altitude <= 39000):
+            if (distance >= 100 or distance <= 1700):
+                
+                #A320neo uses Jet A-1 type fuel, the density is approx 0.8037 kg/L
+                
+                fuel_density = 0.8037
+                
+                fuel_req = rf_model.predict([[distance,altitude, ZFW]])[0]
+                fuel_req_formatted = '{:.0f}'.format(fuel_req)
+                
+                fuel_volume = fuel_req / fuel_density
+                fuel_volume_formatted = '{:.0f}'.format(fuel_volume)
+                
+                #Current fuel price given in rupees per kilo litre, taken from https://iocl.com/aviation-fuel gives price for 4 main airline hubs of india: mumbai, delhi, chennai, kolkata
+                fuel_mumbai = 110592.31 /1000
+                fuel_delhi = 118199.17 /1000
+                fuel_kolkata = 126697.08 / 1000
+                fuel_chennai = 122423.92 / 1000
+                
+                fuel_prices = {
+                'Mumbai': fuel_mumbai,
+                'Delhi': fuel_delhi,
+                'Kolkata': fuel_kolkata,
+                'Chennai': fuel_chennai
+                }
+                
+                fuel_cost = fuel_volume * np.array(list(fuel_prices.values()))
 
-    # Format the fuel cost for each location
-    formatted_fuel_costs = {place: f'{cost:.2f} Rupees' for place, cost in zip(fuel_prices.keys(), fuel_cost)}
+                # Format the fuel cost for each location
+                formatted_fuel_costs = {place: f'{cost:.0f} Rupees' for place, cost in zip(fuel_prices.keys(), fuel_cost)}
 
-    return render_template('result.html', fuel_req=fuel_req, fuel_volume=fuel_volume, fuel_cost=formatted_fuel_costs)
+                return render_template('result.html', fuel_req=fuel_req_formatted, fuel_volume=fuel_volume_formatted, fuel_cost=formatted_fuel_costs)
+            
+        else:
+                raise ValueError('''
+                                 ZFW cannot be below 57000kg, 
+                                 Altitude should be below 39000ft, 
+                                 Distance should be in between 100nm to 1700nm
+                                 ''')
+    
+    except ValueError as e:                 #Common type of exception which can occur
+        return render_template('error.html', error_message=str(e))
+    except Exception as e:                  #Other types of exception which can occur
+        return render_template('error.html', error_message=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
